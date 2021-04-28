1848d724916f167899a22cf3991a6d308e461db6
pbalykov <p.d.balykov@gmail.com>
2021-04-28 08:51:37 +0300

    nejudge

--- skript.sh
+++ skript.sh
@@ -9,6 +9,8 @@ curl —proto '=https' —tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 source ~/.profile 
 git clone https://github.com/pbalykov/nejudge.git
+mkdir nejudge/pos
+mkdir nejudge/rez
 mkdir nejudge/pos/Demo\ contest
 mkdir nejudge/pos/Demo\ contest/1 nejudge/pos/Demo\ contest/2
 mkdir nejudge/rez/Demo\ contest
